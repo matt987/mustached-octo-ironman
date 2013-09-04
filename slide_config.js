a1b65c89d2728288439a8c06d077f8b53525f783
Carlos Mathiasen <carlos@integrafevs.com>
2013-09-04 07:18:11 -0400

    First page commit

---
+++ slide_config.js
@@ -0,0 +1,41 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Ruby On Rails 4',
+    subtitle: "Don't repeat yourself",
+    place: "JOINEA!",
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Carlos Mathiasen(matts)',
+    company: 'Developer',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@reese987',
+    www: '',
+    github: 'http://github.com/matt987'
+  }, {
+    name: 'Leois Linka',
+    company: 'Developer',
+    gplus: '',
+    twitter: '',
+    www: '',
+    github: ''
+  }]
+};
+
